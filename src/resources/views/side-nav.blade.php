69d037787ef4fcc1878cdce9b2250b33ab5218f1
Giacomo <giacomo.asperini@gmail.com>
2020-12-16 10:49:47 +0100

    Changed icon

--- src/resources/views/side-nav.blade.php
+++ src/resources/views/side-nav.blade.php
@@ -1,6 +1,6 @@
 <li class="nav-item">
     <a href="{{ route('killerquotes.index')}}" class="nav-link">
-        <i class="nav-icon fas fa-cash-register"></i>
+        <i class="nav-icon fas fa-file-invoice-dollar"></i>
         <p>Preventivi Killer</p>
     </a>
 </li>
