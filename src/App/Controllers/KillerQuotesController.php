41f9d611e26e55759f19fd654672782ba57b6a55
Giacomo <giacomo.gasperini@gmail.com>
2021-03-02 12:33:23 +0100

    fixed clean to calculate

--- src/App/Controllers/KillerQuotesController.php
+++ src/App/Controllers/KillerQuotesController.php
@@ -144,7 +144,7 @@ class KillerQuotesController extends Controller
             'sconto_value' => 'nullable|numeric',
             'notes' => 'nullable',
             'deal_id' => 'nullable|exists:Deals\App\Models\Deal,id',
-            'accepted' => 'nullable'
+            'accepted' => 'nullable',
         ]);
 
         if($v->fails())
@@ -222,7 +222,7 @@ class KillerQuotesController extends Controller
         if(!empty($data['deal_id']))
             $this->attachToDeal($quote, $data['deal_id']);
 
-        $quote->update(['importo' => $quote->clean_importo]);
+        $quote->update(['importo' => $quote->calculate_importo]);
 
         $company = Company::find($request->company_id);
         if($company->clients()->first()->id == 1)
@@ -347,7 +347,7 @@ class KillerQuotesController extends Controller
             $this->attachToDeal($quote, $data['deal_id']);
         }
 
-        $quote->update(['importo' => $quote->clean_importo]);
+        $quote->update(['importo' => $quote->calculate_importo]);
 
         return redirect(route('killerquotes.edit', $quote->id))->with('message', 'Preventivo Salvato');
     }
